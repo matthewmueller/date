bd332b624628fb04077ef8fc7dfea7a2d86e4dfd
Bulkan Evcimen <bulkan@gmail.com>
2013-08-19 22:24:33 +1000

    correctly parse dates like "1st of March"
    fixes MatthewMueller/date#10

--- test/parser.js
+++ test/parser.js
@@ -603,19 +603,31 @@ describe('parse context if its a string (fixes: #38)', function () {
  */
 
 describe('months (fixes: #10)', function (){
+  var after = new Date('May 13, 2013 13:30:00');
   it('2nd of January', function () {
-    var after = new Date('May 13, 2013 13:30:00');
     var date = parse('2nd of January 12:30', after);
     assert('12:30:00' == t(date));
     assert('1/2/13' == d(date));
   });
 
   it('1st of March', function () {
-    var after = new Date('May 13, 2013 13:30:00');
     var date = parse('1st of March', after);
     assert('13:30:00' == t(date));
-    assert('1/3/13' == d(date));
+    assert('3/1/13' == d(date));
   });
+
+  it('31st of September 4:00am', function () {
+    var date = parse('31st of September 4:00am', after);
+    assert('4:00:00' == t(date));
+    assert('9/31/13' != d(date));
+    assert('9/30/13' == d(date));
+  });
+
+  it('first day of the January', function(){
+    var date = parse('1st of January 4:00am', after);
+    assert('4:00:00' == t(date));
+    assert('1/1/13' == d(date));
+  })
 });
 
 /**
